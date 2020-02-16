f0eab426a44ceb9539597b0a29d13c5783d9439a
MyNa Do <myna.do@mahoyo.com>
2020-02-16 13:14:22 +0100

    fixed the navMenu so it doesn't disappear in desktop view

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,38 +9,35 @@ const isInViewport = (e) => {
 		project.style.display = "block";
 		project.classList.add("popin");
 		project.classList.remove("hidden");
-		project.classList.add('random-position');
 	}
 });
 }
 
 window.addEventListener('scroll',isInViewport);
 
-  /* Open when someone clicks on the span element */
 const openNav = () => {
 	document.querySelector("#navMenu").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
+}
+
 const closeNav = () => {
 	document.querySelector("#navMenu").style.width = "0%";
-  } 
-
+}
 
 if (document.documentElement.clientWidth < 900) {
 	document.querySelector('#open-menu').addEventListener('click', e => {
 		e.preventDefault();
-		console.log('click');
 		openNav();
 	});
 	
 	document.querySelector('#close-menu').addEventListener('click', e => {
 		e.preventDefault();
-		console.log('click');
 		closeNav();
 	});
 	document.querySelectorAll('#navLinks li').forEach(link => link.addEventListener('click', e => {
-		console.log('click');
 		closeNav();
 	}));
+}
+
+if (document.documentElement.clientWidth > 900) {
+	openNav();
 }
